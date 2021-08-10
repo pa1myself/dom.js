78cbb6d8994f587d07b627abb8d68687aeedcd59
pa1myself <kumarpavam869.com>
2021-08-10 18:06:41 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,73 @@
+console.dir(document);
+console.log(document.domain);
+console.log(document.title);
+console.log(document.doctype);
+console.log(document.all);
+console.log(document.all[10]);
+console.log(document.all[12]);
+console.log(document.forms);
+console.log(document.links);
+
+//console.log(document.getElementById('header-title'));
+var headertitle = document.getElementById('header-title');
+//console.log(headertitle);
+//headertitle.textContent = 'hello'
+
+var items = document.getElementsByClassName('list-group-item');
+//console.log(items);
+//console.log(items[3]);
+
+var li = document.getElementsByTagName('li');
+//console.log(li);
+//console.log(li[2]);
+
+var header = document.querySelector('#main-header');
+//header.style.borderbottom = 'solid 4px #ccc';
+
+var input = document.querySelector('input');
+//input.value = 'hello world'
+
+var item = document.querySelector('.list-group-item');
+//item.style.color = 'orange';
+
+var titles = document.querySelectorAll('.title');
+console.log(titles);
+//titles[1].textContent = 'hello';
+
+
+var itemList = document.querySelector('#items');
+//console.log(itemList.parentNode);
+//itemList.parentNode.style.backgroundColor = '#f4f4f4';
+
+//console.log(itemList.parentNode);
+//itemList.parentNode.style.backgroundColor = '#f4f4f4';
+
+//console.log(itemList.children);
+//console.log(itemList.children[2]);
+//itemList.children[2].style.backgroundColor = 'yellow';
+
+//console.log(itemList.firstChild);
+//console.log(itemList.firstElementChild);
+
+//console.log(itemList.lastChild);
+//console.log(itemList.lastElementChild);
+
+//console.log(itemList.previousElementSibling);
+//itemList.previousElementSibling.style.color = 'green';
+
+var button = document.getElementById('button').addEventListener
+//('click', buttonClick);
+
+//function buttonClick(e){
+    // console.log(e);
+    // console.log(e.target);
+
+    
+//console.log(e.altKey);
+//console.log(e.ctrlKey);
+//console.log(e.shiftKey);
+
+
+
+
+
